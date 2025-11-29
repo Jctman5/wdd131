2b88a22cf180a0dff7bfcded2f9f40ed55ff130e
Jctman5 <Jctman5@gmail.com>
2025-11-29 16:52:43 -0500

    Update temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -18,45 +18,67 @@ const temples = [
   {
     name: "Yigo Guam",
     location: "Yigo, Guam",
-    dedicated: "2020, May, 2",
+    dedicated: "2022, May, 22",
     area: 6861,
-    imageUrl: "images/yigo-guam-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/yigo-guam-temple/yigo-guam-temple-26495-main.jpg"
   },
   {
     name: "Washington D.C.",
     location: "Kensington, Maryland, United States",
     dedicated: "1974, November, 19",
     area: 156558,
-    imageUrl: "images/washington-dc-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/washington-d.c.-temple/washington-d.c.-temple-14992-main.jpg"
   },
   {
     name: "Lima Perú",
     location: "Lima, Perú",
     dedicated: "1986, January, 10",
     area: 9600,
-    imageUrl: "images/lima-peru-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/lima-peru-temple/lima-peru-temple-12721-main.jpg"
   },
   {
     name: "Mexico City Mexico",
     location: "Mexico City, Mexico",
     dedicated: "1983, December, 2",
     area: 116642,
-    imageUrl: "images/mexico-city-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/mexico-city-mexico-temple/mexico-city-mexico-temple-4060-main.jpg"
   },
   {
     name: "Rome Italy",
     location: "Rome, Italy",
     dedicated: "2019, March, 10",
     area: 40000,
-    imageUrl: "images/rome-italy-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-2642-main.jpg"
   },
   {
     name: "Seattle Washington",
     location: "Bellevue, Washington, United States",
     dedicated: "1980, November, 17",
     area: 110000,
-    imageUrl: "images/seattle-washington-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/seattle-washington-temple/seattle-washington-temple-55800-main.jpg"
+  },
+  {
+    name: "Richmond Virginia",
+    location: "Glen Allen, Virginia, United States",
+    dedicated: "2023, May 7",
+    area: 39202,
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/richmond-virginia-temple/richmond-virginia-temple-32557-main.jpg"
+  },
+  {
+    name: "Oakland California",
+    location: "Oakland California",
+    dedicated: "1964, November 17",
+    area: 80157,
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/oakland-california-temple/oakland-california-temple-2654-main.jpg"
+  },
+  {
+    name: "Raleigh North Carolina",
+    location: "Raleigh North Carolina",
+    dedicated: "1999, December 18",
+    area: 12864,
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/raleigh-north-carolina-temple/raleigh-north-carolina-temple-6727-main.jpg"
   }
+    
 ];
 
 
